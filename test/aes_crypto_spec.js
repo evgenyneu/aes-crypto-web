4baf372788a6b9dcfb2680330eb06c68c1ecdd50
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-05-22 18:54:44 +1000

    Test for aes crypto

--- test/aes_crypto_spec.js
+++ test/aes_crypto_spec.js
@@ -11,6 +11,12 @@
       var result = aesCrypto.decrypt('AESCryptoV108f46e2fb15f50e9c170442ec5ec70e6fcded6378b13f1a659f0eb65e8eddb2335de8e76be90b2f0a', 'test');
       expect(result).toEqual('My Test Message 日本');
     });
+
+    it('encrypts and decypts', function(){
+      var ecrypted = aesCrypto.encrypt('Some text 日本', 'My password 日本');
+      var decrypted = aesCrypto.decrypt(ecrypted, 'My password 日本');
+      expect(decrypted).toEqual('Some text 日本');
+    });
   });
 
 })(this);
