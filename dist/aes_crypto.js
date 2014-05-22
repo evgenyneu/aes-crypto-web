03491a1c621be9b03a33a9a44b4f8b57d52d2f1f
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-05-22 20:58:46 +1000

    Encrypt/decrypt JS

--- dist/aes_crypto.js
+++ dist/aes_crypto.js
@@ -37,6 +37,44 @@ var aesCrypto = {};
     } catch (err) { return ''; }
   };
 }(aesCrypto));
+(function () {
+  'use strict';
+
+  function trimString(str) {
+    return str.replace(/^\s+/, '').replace(/\s+$/, '');
+  }
+
+  var passwordField = document.getElementById('PasswordField');
+  var messageField = document.getElementById('MessageField');
+  var encryptButton = document.getElementById('EncryptButton');
+  var decryptButton = document.getElementById('DecryptButton');
+
+
+  encryptButton.onclick = function() {
+    if (!messageField.value || !passwordField.value) { return; }
+
+    var encrypted = aesCrypto.encrypt(
+      trimString(messageField.value),
+      trimString(passwordField.value));
+
+    messageField.value = encrypted;
+
+    return false;
+  };
+
+  decryptButton.onclick = function() {
+    if (!messageField.value || !passwordField.value) { return; }
+
+    var decrypted = aesCrypto.decrypt(
+      trimString(messageField.value),
+      trimString(passwordField.value));
+
+    if (decrypted) { messageField.value = decrypted; }
+
+    return false;
+  };
+
+})();
 /*
 CryptoJS v3.1.2
 code.google.com/p/crypto-js
